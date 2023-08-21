43d558e1e3d7630902a97dd48e5ffb7050d9e47a
mikeermatinger <118219882+mikeermatinger@users.noreply.github.com>
2023-08-21 16:01:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,191 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+
+// Items array
+const items = [
+    {name: "bear", clan: "makwa", image: "bear.png"},
+    {name: "deer", clan: "waawaashkeshi", image: "deer.png"},
+    {name: "eagle", clan: "mgizi", image: "eagle.png"},
+    {name: "fish", clan: "giigoonh", image: "fish.png"},
+    {name: "wolf", clan: "ma'iingan", image: "wolf.png"},
+    {name: "bird", clan: "bneshiinh", image: "bird.png"},
+    {name: "turtle", clan: "mikinaak", image: "turtle.jpeg"},
+    {name: "beaver", clan: "mik", image: "beaver.png"},
+    {name: "crane", clan: "jijaak", image: "crane.png"},
+    {name: "loon", clan: "maang", image: "loon.png"},
+    {name: "fisher", clan: "jiik", image: "fisher.png"},
+    {name: "heron", clan: "shagi", image: "heron.png"},
+    {name: "snipe", clan: "btashkaanzhe", image: "snipe.png"},
+    {name: "hawk", clan: "memeskniniisi", image: "hawk.png"},
+];
+
+const medicinewheel = [
+    {name: "medicinewheel", image: "medicinewheelsmaller.png"},
+];
+
+
+// Initial Time
+let seconds = 0,
+    minutes = 0;
+
+
+// Initial Moves & Win Count
+let movesCount = 0,
+    winCount = 0;
+
+
+// For Timer
+const timeGenerator = () => {
+    seconds += 1;
+    // Minutes logic
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+    // Format time before displaying
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
+
+// Calculate moves
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+// Pick random objects from item array
+const generateRandom = (size = 4) => {
+    // Temporary array
+    let tempArray = [...items];
+    // Initializes cardValues array
+    let cardValues = [];
+    // Size should be double (4*4 matrix)/2 since pairs of objects would exist
+    size = (size * size) / 2;
+    // Random object selection
+    for (let i = 0; i < size; i++) {
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
+        cardValues.push(tempArray[randomIndex]);
+        // Once selected remove the object from temp array
+        tempArray.splice(randomIndex, 1);
+    }
+    return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = "";
+    cardValues = [...cardValues, ...cardValues];
+    // Simple shuffle
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+        /*
+            Create Cards
+            before => front side (contains question mark)
+            after => back side (contains actual image)
+            data-card-value is a custom attribute which stores the names of the cards to match later
+        */
+        gameContainer.innerHTML += `
+            <div class="card-container" data-card-value="${cardValues[i].name}">
+                <div class="card-before"><img src="${medicinewheel[0].image}" class="image"></div>
+                <div class="card card-after"><p class="card-title m3">${cardValues[i].clan}</p><img src="${cardValues[i].image}" class="image"><p class="card-text m3">${cardValues[i].name}</p></div>
+            </div>
+            `;
+    }
+    // Grid
+    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+    // Cards
+    cards = document.querySelectorAll(".card-container");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            // If selected card is not matched yet, then only run (ie only matched card when clicked, would be ignored)
+            if (!card.classList.contains("matched")) {
+                // Flip the clicked card
+                card.classList.add("flipped");
+                // If it is the firstcard (!firstCard because first card will initially false)
+                if (!firstCard) {
+                    // So current card will become firstCard
+                    firstCard = card;
+                    // Current cards value becomes fisrtCardValue
+                    firstCardValue = card.getAttribute("data-card-value");
+                }
+                else {
+                    // Increment moves since user selected second card
+                    movesCounter();
+                    // secondCard and value
+                    secondCard = card;
+                    let secondCardValue = card.getAttribute("data-card-value");
+                    if (firstCardValue == secondCardValue) {
+                        // If both cards match add matched class so these cards would be ignored next time
+                        firstCard.classList.add("matched");
+                        secondCard.classList.add("matched");
+                        // Set firstCard to falsesince next card will be first now
+                        firstCard = false;
+                        // winCount increment as user found a correct match
+                        winCount += 1;
+                        // Check if winCount == half of cardValues
+                        if (winCount == Math.floor(cardValues.length / 2)) {
+                            result.innerHTML = `<h2>You Won</h2> <h4>Moves: ${movesCount}</h4>`;
+                            stopGame();
+                        }
+                    }
+                    else {
+                        // If the cards do not match
+                        // Flip the cards back to normal
+                        let [tempFirst, tempSecond] = [firstCard, secondCard];
+                        firstCard = false;
+                        secondCard = false;
+                        let delay = setTimeout(() => {
+                            tempFirst.classList.remove("flipped");
+                            tempSecond.classList.remove("flipped");
+                        }, 900);
+                    }
+                }
+            }
+        });
+    });
+};
+
+// Start game
+startButton.addEventListener("click", () => {
+    movesCount = 0;
+    seconds = 0;
+    minutes = 0;
+    // Controls and button visibility
+    controls.classList.add("hide");
+    stopButton.classList.remove("hide");
+    startButton.classList.add("hide");
+    // Start timer
+    interval = setInterval(timeGenerator, 1000);
+    // initial moves
+    moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+    initializer();
+});
+
+// Stop game
+stopButton.addEventListener("click", (stopGame = () => {
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+    })
+);
+
+// Initialize values and function calls
+const initializer = () => {
+    result.innerText = "";
+    wincount = 0;
+    let cardValues = generateRandom();
+    console.log(cardValues);
+    matrixGenerator(cardValues);
+};
